9129912b71254285dbbce41533f9b8ac7ebf0736
Rich Hill <U452675@AD001.infoFTPS.COM>
2016-04-09 14:11:23 -0400

    pulling changes

--- app/pages/hello-ionic/hello-ionic.js
+++ app/pages/hello-ionic/hello-ionic.js
@@ -1,10 +1,16 @@
+<<<<<<< HEAD
 import {Page, NavController} from 'ionic-angular';
 
+=======
+import {Page, Platform} from "ionic-angular";
+import {FbProvider} from "../../providers/fb-provider/fb-provider";
+>>>>>>> origin/master
 
 @Page({
-  templateUrl: 'build/pages/hello-ionic/hello-ionic.html'
+    templateUrl: 'build/pages/hello-ionic/hello-ionic.html'
 })
 export class HelloIonicPage {
+<<<<<<< HEAD
 	  constructor(nav: NavController){
 		    this.nav = nav;
 		  }
@@ -14,5 +20,30 @@ export class HelloIonicPage {
 	    // causing the nav controller to animate the new page in
 	    this.nav.push(InterestsPage);
 	  }
+=======
+    static get parameters() {
+        return [[Platform], [FbProvider]];
+    }
+
+    constructor(platform, fbProvider) {
+        console.log(fbProvider);
+        this.platform = platform;
+        this.fb = fbProvider;
+        this.email = '';
+        this.name = '';
+        this.id = '';
+    };
+
+    login() {
+        //console.log(this.fb);
+        this.fb.login().then(() => {
+            this.fb.getCurrentUserProfile().then((profileData) => {
+                this.email = profileData.email;
+                this.name = profileData.name;
+                this.id = profileData.id;
+            });
+        });
+    };
+>>>>>>> origin/master
 
 }
