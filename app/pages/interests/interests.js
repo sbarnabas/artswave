cb3848c6aa037b0e8ac7a134cc5589760860773e
Rich Hill <U452675@AD001.infoFTPS.COM>
2016-04-09 16:36:01 -0400

    setting up the grid of interests

--- app/pages/interests/interests.js
+++ app/pages/interests/interests.js
@@ -9,15 +9,29 @@ export class InterestsPage {
   static get parameters() {
     return [[NavController], [NavParams], [Http]];
   }
+  
+  saveInterests() {
+	  console.log("Saved your interests");
+  };
+
 
   constructor(nav, navParams, http) {
     this.nav = nav;
     this.http = http;
 
-    console.log("getting the interestes");
-   this.http.get("http://localhost:3000/interests").map(res => res.json()).subscribe(data => {
-      console.log("got the interests ",data);
-    });
+    console.log("getting the interests");
+//   this.http.get("http://localhost:3000/interests").map(res => res.json()).subscribe(data => {console.log("got the interests ",data);});
+    this.interests = [
+                     {"name":"Painting", "imageUri":"https://artspass.theartswave.org/wp-content/themes/artspass/images/icons/color47/Arts_Center.png", "colorValue" : "F7941E"}, 
+                     {"name":"Theatre", "imageUri":"https://artspass.theartswave.org/wp-content/themes/artspass/images/icons/color48/Theatre.png", "colorValue" : "6CB33F"}, 
+                     {"name":"Dance", "imageUri":"https://artspass.theartswave.org/wp-content/themes/artspass/images/icons/color46/Dance.png", "colorValue" : "552988"}, 
+                     {"name":"Museum", "imageUri":"https://artspass.theartswave.org/wp-content/themes/artspass/images/icons/color45/Museum.png", "colorValue" : "00AEEF"}, 
+                     {"name":"Music", "imageUri":"https://artspass.theartswave.org/wp-content/themes/artspass/images/icons/color47/Music.png", "colorValue" : "F7941E"}, 
+                     {"name":"Culinary", "imageUri":"https://artspass.theartswave.org/wp-content/themes/artspass/images/icons/color2/Restaurant.png", "colorValue" : "EE3524"}, 
+                     {"name":"Hiking", "imageUri":"https://artspass.theartswave.org/wp-content/themes/artspass/images/icons/color2/cincy_perks.png", "colorValue" : "EE3524"}, 
+                     {"name":"3D Art", "imageUri":"https://artspass.theartswave.org/wp-content/themes/artspass/images/icons/color46/Retail.png", "colorValue" : "552988"}, 
+                     {"name":"Sports", "imageUri":"https://artspass.theartswave.org/wp-content/themes/artspass/images/icons/color2/hot.png", "colorValue" : "EE3524"}
+                 ];
 //    .then(function(response){ this.interests = response.data; });
 
 /*
