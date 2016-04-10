065431888cbd03e00b79863d520e4ea73a19eb4d
Rich Hill <U452675@AD001.infoFTPS.COM>
2016-04-10 11:02:18 -0400

    completing quotes and roles

--- app/pages/about/about.js
+++ app/pages/about/about.js
@@ -17,9 +17,9 @@ export class AboutPage {
                          {"name":"Teresa Martinez", "avatarImage":"teresa.jpg", "quote":"I honestly didn't know algorithmically was a word", "role" : "The Pitcher"}, 
                          {"name":"Sandy Barnabas", "avatarImage":"sandy.jpg", "quote":"I don't know what art is. But I think I know it when I see it.", "role" : "Lead Drinker / Developer"}, 
                          {"name":"Rich Hill", "avatarImage":"rich.jpg", "quote":"Coffee is for closers!", "role" : "Beer Supplier / Architect"}, 
-                         {"name":"Kaleb McKelvey", "avatarImage":"kaleb.jpg", "quote":"", "role" : ""}, 
-                         {"name":"Stephen Pestian", "avatarImage":"stephen.jpg", "quote":"", "role" : ""}, 
-                         {"name":"Melinda Hershey", "avatarImage":"melinda.jpg", "quote":"", "role" : "Lord of the Slides"}, 
+                         {"name":"Kaleb McKelvey", "avatarImage":"kaleb.jpg", "quote":"Music is life.", "role" : "Developer"}, 
+                         {"name":"Stephen Pestian", "avatarImage":"stephen.jpg", "quote":"FUTURE.", "role" : "DJ / Wine Merchant / User Experience"}, 
+                         {"name":"Melinda Hershey", "avatarImage":"melinda.jpg", "quote":"Teamwork makes dreamwork", "role" : "Lord of the Slides"}, 
                          {"name":"Mary Kate Genis", "avatarImage":"mary_kate.jpg", "quote":"No. Monet!", "role" : "Mock Master"}, 
                          {"name":"Tiffany White", "avatarImage":"tiffany.jpg", "quote":"Team no breaks!", "role" : "Jack of All Trades"}
                      ];
