ca376f9a2833a9a2e2b77f8a55e393f91609fff7
Sandy Barnabas <sbarnabas@gmail.com>
2016-04-10 11:02:36 -0400

    tabs

--- app/pages/event-map/event-map.js
+++ app/pages/event-map/event-map.js
@@ -2,9 +2,9 @@ import {Page, NavController, NavParams} from "ionic-angular";
 import {ItemDetailsPage} from "../item-details/item-details";
 
 @Page({
-    templateUrl: 'build/pages/discover/discover.html'
+    templateUrl: 'build/pages/event-map/event-map.html'
 })
-export class DiscoverPage {
+export class EventMapPage {
     static get parameters() {
         return [[NavController], [NavParams]];
     }
@@ -13,36 +13,6 @@ export class DiscoverPage {
         this.nav = nav;
 
         // If we navigated to this page, we will have an item available as a nav param
-        this.selectedItem = navParams.get('item');
-        this.items = [];
-        this.getData();
-        if (this.data.events && this.data.events.event) {
-            try {
-                for (var idx in this.data.events.event) {
-                    var d = this.data.events.event[idx]
-                    this.items.push({
-                        title: d.eventName ? d.eventName : "(Untitled)",
-                        image: d.eventImage ? d.eventImage : "",
-                        note:  d.eventDescription ? d.eventDescription : "None"
-                    });
-                }
-
-            } catch (ex) {
-                console.log(ex);
-            }
-        }
-    }
-
-    itemRemove(event, item) {
-        var idx = this.items.indexOf(item);
-        if (idx > -1) {
-            this.items.splice(idx, 1);
-        }
-    }
-
-    itemTapped(event, item) {
-        this.nav.push(ItemDetailsPage, {
-            item: item
-        });
+    
     }
 }
