ea7c42f8885617f0bb6b199159bdd1d43b884cc2
Rich Hill <U452675@AD001.infoFTPS.COM>
2016-04-10 10:52:43 -0400

    adding avatar images, quotes, and roles

--- app/pages/about/about.js
+++ app/pages/about/about.js
@@ -14,14 +14,14 @@ export class AboutPage {
     this.nav = nav;
 
     this.contributors = [
-                         {"name":"Teresa Martinez", "avatarImage":"teresa.jpg"}, 
-                         {"name":"Sandy Barnabas", "avatarImage":"sandy.jpg"}, 
-                         {"name":"Rich Hill", "avatarImage":"rich.jpg"}, 
-                         {"name":"Kaleb McKelvey", "avatarImage":"kaleb.jpg"}, 
-                         {"name":"Stephen Pestian", "avatarImage":"stephen.jpg"}, 
-                         {"name":"Melinda Hershey", "avatarImage":"melinda.jpg"}, 
-                         {"name":"Mary Kate Genis", "avatarImage":"mary_kate.jpg"}, 
-                         {"name":"Tiffany White", "avatarImage":"tiffany.jpg"}
+                         {"name":"Teresa Martinez", "avatarImage":"teresa.jpg", "quote":"I honestly didn't know algorithmically was a word", "role" : "The Pitcher"}, 
+                         {"name":"Sandy Barnabas", "avatarImage":"sandy.jpg", "quote":"I don't know what art is. But I think I know it when I see it.", "role" : "Lead Drinker / Developer"}, 
+                         {"name":"Rich Hill", "avatarImage":"rich.jpg", "quote":"Coffee is for closers!", "role" : "Beer Supplier / Architect"}, 
+                         {"name":"Kaleb McKelvey", "avatarImage":"kaleb.jpg", "quote":"", "role" : ""}, 
+                         {"name":"Stephen Pestian", "avatarImage":"stephen.jpg", "quote":"", "role" : ""}, 
+                         {"name":"Melinda Hershey", "avatarImage":"melinda.jpg", "quote":"", "role" : "Lord of the Slides"}, 
+                         {"name":"Mary Kate Genis", "avatarImage":"mary_kate.jpg", "quote":"No. Monet!", "role" : "Mock Master"}, 
+                         {"name":"Tiffany White", "avatarImage":"tiffany.jpg", "quote":"Team no breaks!", "role" : "Jack of All Trades"}
                      ];
   }
 }
