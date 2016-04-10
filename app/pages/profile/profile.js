b38ebcc575716dad835d1cbf1101ddb7ab488243
Rich Hill <U452675@AD001.infoFTPS.COM>
2016-04-10 14:29:01 -0400

    update profile

--- app/pages/profile/profile.js
+++ app/pages/profile/profile.js
@@ -19,5 +19,6 @@ export class ProfilePage {
     this.nav = nav;
 
     this.userProfile = {"userName": "Claude", "level" : "Legendary", "points": "20600", "interests": "Painting", "events" : "Tours!", "favoriteThing" : "Starry Night", "experienceCount" : "8,675"};
+    this.connectedPrograms = [{"programName": "ArtsWave Cincinnati", "programImageUri" : "img/icon.png"}, {"programName": "Greater Columbus Arts Council", "programImageUri" : "img/gcac.png"}];
   }
 }
